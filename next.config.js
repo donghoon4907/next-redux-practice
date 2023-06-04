89138213aa242c56929a7b6ec4321bd884314c85
이동훈 <wre789812@naver.com>
2023-06-04 21:25:53 +0900

    connect storybook

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const Dotenv = require('dotenv-webpack');
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    compiler: {
-        styledComponents: true,
-    },
 };
 
 module.exports = {
